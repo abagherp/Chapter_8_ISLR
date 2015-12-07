fbb22ec291356fc50bd5b9c22cdb3f7c6e6ddf70
abagherp <bagherpour.unb@gmail.com>
2015-12-07 09:40:49 -0400

    Prediction with tree and Random Forest

--- Lab_Decision_Tree.R
+++ Lab_Decision_Tree.R
@@ -16,5 +16,15 @@ train = sample(1:nrow(Carseats),250)
 tree.carseats = tree(High~.-Sales,data=Carseats,subset = train)
 plot(tree.carseats)
 text(tree.carseats,pretty = 1)
+tree.pred = predict(tree.carseats,Carseats[-train,],type = "class")
+with(Carseats[-train,],table(tree.pred,High))
 
+cv.carseats = cv.tree(tree.carseats,FUN = prune.misclass)
+cv.carseats
+plot(cv.carseats)
+prune.carseats = prune.misclass(tree.carseats,best=13)
+plot(prune.carseats)
+text(prune.carseats,pretty = 1)
 
+tree.pred = predict(tree.carseats,Carseats[-train,],type = "class")
+with(Carseats[-train,],table(tree.pred,High))
